02f39063b6a1622b4275d1dc67ab559d29c0f2ed
Shivaraj Bakale <shivaraj.sb@byjus.com>
2022-08-01 15:16:02 +0530

    Added the changes for the console statement

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * as Button from './components/atoms/button';
 export * as Input from './components/atoms/input';
 export * as Chip from './components/atoms/chip';
+console.log('Some random console statement');
